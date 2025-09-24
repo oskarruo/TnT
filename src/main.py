6ae6c52ab892fc2229fc03ef9290e0e70a7004ff
oskarruo <oskar.ruokolainen@gmail.com>
2025-09-24 23:24:53 +0300

    added a main.py for running scraping & analyzing from the command line

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import sys
+import glob
+import os
+import pandas as pd
+import json
+from analyze import analyze
+
+
+# merges the csv files created by the analyze function, and joins the speech data collected into speeches.json, saves everything into analysis.csv
+def merge_and_join():
+    csv_files = glob.glob(os.path.join("../data/csv", "*.csv"))
+    if not csv_files:
+        print("No CSVs to merge")
+        return
+    try:
+        dfs = []
+        for f in csv_files:
+            df = pd.read_csv(f, index_col=False)
+            df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
+            dfs.append(df)
+        merged_df = pd.concat(dfs, ignore_index=True)
+        with open("../data/speeches.json", "r", encoding="utf-8") as f:
+            json_data = json.load(f)
+        json_df = pd.DataFrame(json_data)
+        merged_df = pd.merge(merged_df, json_df, on="slug", how="left")
+        merged_csv_path = os.path.join("../data/csv", "analysis.csv")
+        merged_df.to_csv(merged_csv_path, index=False)
+        print("Merged CSVs and wrote to data/csv/analysis.csv")
+        for f in csv_files:
+            os.remove(f)
+    except Exception as e:
+        print("Error merging:", e)
+        return
+
+
+def main(n_speeches, n_per_time, sorting):
+    analyze(n_speeches, n_per_time, sorting)
+    merge_and_join()
+
+
+# Usage: python main.py [n: int (amount of speeches to download)] [n_per_time: int (amount of speeches to download and analyze at once)] [sorting :string (sort by "popular" or "newest" speeches)]
+if __name__ == "__main__":
+    n_speeches = 10
+    n_per_time = 5
+    sorting = "popular"
+    if len(sys.argv) > 1:
+        n_speeches = int(sys.argv[1])  # number of speeches
+    if len(sys.argv) > 2:
+        n_per_time = int(sys.argv[2])  # how many speeches are processed at once
+    if len(sys.argv) > 3:
+        sorting = sys.argv[3]  # "popular" for popular and "newest" for newest
+    main(n_speeches, n_per_time, sorting)
