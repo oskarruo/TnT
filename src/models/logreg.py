c4291e5d95fac3fc1a9a1d1b627e7f172cdbbb11
oskarruo <oskar.ruokolainen@gmail.com>
2025-10-14 17:58:56 +0300

    added logreg and randomforest models

---
+++ src/models/logreg.py
@@ -0,0 +1,73 @@
+import glob
+import pandas as pd
+import re
+import os
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+class LogReg:
+    def __init__(self):
+        csv_files = glob.glob("../../data/csv/*.csv")
+        playlists = pd.DataFrame()
+        teds = pd.DataFrame()
+
+        for file in csv_files:
+            if re.match(r"^analyzed_playlist", os.path.basename(file)):
+                playlists = pd.concat([playlists, pd.read_csv(file)], ignore_index=True)
+            elif re.match(r"^analyzed_speeches", os.path.basename(file)):
+                teds = pd.concat([teds, pd.read_csv(file)], ignore_index=True)
+
+        playlists = playlists.drop_duplicates(subset=["url"])
+        teds = teds.drop_duplicates(subset=["slug"])
+        teds = teds[teds["type_id"] == 1]
+
+        columns_to_use = ["rate_of_speech", "articulation_rate", "balance", "f0_std"]
+        x_playlists = playlists[columns_to_use]
+        x_teds = teds[columns_to_use]
+
+        if len(x_playlists) > len(x_teds):
+            x_playlists = x_playlists.sample(n=len(x_teds))
+        elif len(x_teds) > len(x_playlists):
+            x_teds = x_teds.sample(n=len(x_playlists))
+
+        x = pd.concat([x_playlists, x_teds], ignore_index=True)
+        y = [0] * len(x_playlists) + [1] * len(x_teds)
+
+        x_train, x_test, y_train, self.y_test = train_test_split(
+            x, y, test_size=0.2, random_state=1
+        )
+
+        x_train_const = sm.add_constant(x_train)
+        x_test_const = sm.add_constant(x_test)
+
+        self.model = sm.Logit(y_train, x_train_const).fit(disp=False)
+
+        self.y_pred_prob = self.model.predict(x_test_const)
+        self.y_pred = (self.y_pred_prob >= 0.5).astype(int)
+
+    def summary(self):
+        summary_df = pd.concat(
+            [
+                self.model.params.rename("coef"),
+                self.model.pvalues.rename("p_value"),
+                self.model.bse.rename("std_err"),
+                self.model.conf_int().rename(columns={0: "ci_lower", 1: "ci_upper"}),
+                pd.Series(
+                    self.model.llf, index=self.model.params.index, name="log_likelihood"
+                ),
+                pd.Series(self.model.aic, index=self.model.params.index, name="AIC"),
+                pd.Series(self.model.bic, index=self.model.params.index, name="BIC"),
+                pd.Series(
+                    self.model.prsquared,
+                    index=self.model.params.index,
+                    name="pseudo_R2",
+                ),
+            ],
+            axis=1,
+        )
+        return summary_df
+
+    def get_metrics(self):
+        return classification_report(self.y_test, self.y_pred, output_dict=True)
---
+++ src/models/randomforest.py
@@ -0,0 +1,53 @@
+import glob
+import pandas as pd
+import re
+import os
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+
+
+class RandomForest:
+    def __init__(self):
+        csv_files = glob.glob("../../data/csv/*.csv")
+        playlists = pd.DataFrame()
+        teds = pd.DataFrame()
+
+        for file in csv_files:
+            if re.match(r"^analyzed_playlist", os.path.basename(file)):
+                playlists = pd.concat([playlists, pd.read_csv(file)], ignore_index=True)
+            elif re.match(r"^analyzed_speeches", os.path.basename(file)):
+                teds = pd.concat([teds, pd.read_csv(file)], ignore_index=True)
+
+        playlists = playlists.drop_duplicates(subset=["url"])
+        teds = teds.drop_duplicates(subset=["slug"])
+        teds = teds[teds["type_id"] == 1]
+
+        columns_to_use = ["rate_of_speech", "articulation_rate", "balance", "f0_std"]
+        x_playlists = playlists[columns_to_use]
+        x_teds = teds[columns_to_use]
+
+        if len(x_playlists) > len(x_teds):
+            x_playlists = x_playlists.sample(n=len(x_teds))
+        elif len(x_teds) > len(x_playlists):
+            x_teds = x_teds.sample(n=len(x_playlists))
+
+        x = pd.concat([x_playlists, x_teds], ignore_index=True)
+        y = [0] * len(x_playlists) + [1] * len(x_teds)
+
+        x_train, x_test, y_train, self.y_test = train_test_split(
+            x, y, test_size=0.2, random_state=1
+        )
+
+        param_grid = {
+            "n_estimators": [100, 300, 500],
+            "max_depth": [None, 30, 50],
+        }
+
+        grid = GridSearchCV(RandomForestClassifier(random_state=1), param_grid)
+        grid.fit(x_train, y_train)
+        self.model = grid.best_estimator_
+        self.y_pred = self.model.predict(x_test)
+
+    def get_metrics(self):
+        return classification_report(self.y_test, self.y_pred, output_dict=True)
